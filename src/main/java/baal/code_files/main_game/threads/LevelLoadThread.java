abde43682923b492a4d58d4f5d6c02a9330d931f
baal <rydolf_sikorski@mail.ru>
2022-01-23 22:08:41 +0400

    заменил вызовы на модель игры во всех потоках и контроллере, кроме счётчика тиков игры крашится при запуске

--- src/main/java/baal/code_files/main_game/threads/LevelLoadThread.java
+++ src/main/java/baal/code_files/main_game/threads/LevelLoadThread.java
@@ -3,8 +3,7 @@ package baal.code_files.main_game.threads;
 import baal.ApplicationContextProvider;
 import baal.code_files.level_system.builder_system.LevelBuilderDirectorInterface;
 import baal.code_files.level_system.load_system.LevelsLoadControllerInterface;
-import baal.code_files.main_game.Main_game_controller;
-import lombok.Setter;
+import baal.code_files.main_game.GameModel;
 import lombok.SneakyThrows;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -15,28 +14,25 @@ import java.io.IOException;
 @Scope("prototype")
 public class LevelLoadThread extends Thread {
     private final ApplicationContextProvider applicationContextProvider;
-    @Setter private Main_game_controller main_game_controller;
+    private final GameModel gameModel;
     private final LevelBuilderDirectorInterface levelBuilderDirector;
 
     public LevelLoadThread(ApplicationContextProvider applicationContextProvider,
-                           LevelsLoadControllerInterface levelsLoadController, LevelBuilderDirectorInterface levelBuilderDirector) {
+                           LevelsLoadControllerInterface levelsLoadController, GameModel gameModel, LevelBuilderDirectorInterface levelBuilderDirector) {
         this.applicationContextProvider = applicationContextProvider;
+        this.gameModel = gameModel;
         this.levelBuilderDirector = levelBuilderDirector;
     }
     
     @SneakyThrows
     @Override
     public void run(){
-        setMain_game_controller(this.applicationContextProvider
-                .getApplicationContext()
-                .getBean(Main_game_controller.class));
-
         loadFromThisLevel();
         this.setName("load");
     }
 
     private void loadFromThisLevel() throws IOException, ClassNotFoundException {
-        this.main_game_controller.curr_level =
-                levelBuilderDirector.build(this.main_game_controller.currLevelFilePath);
+        this.gameModel.curr_level =
+                levelBuilderDirector.build(this.gameModel.currLevelFilePath);
     }
 }
--- src/main/java/baal/code_files/main_game/threads/TriggerCheckThread.java
+++ src/main/java/baal/code_files/main_game/threads/TriggerCheckThread.java
@@ -3,9 +3,8 @@ package baal.code_files.main_game.threads;
 import baal.ApplicationContextProvider;
 import baal.code_files.entity_movement.Collision;
 import baal.code_files.level_system.event.Event;
+import baal.code_files.main_game.GameModel;
 import baal.code_files.main_game.LevelChange;
-import baal.code_files.main_game.Main_game_controller;
-import lombok.Setter;
 import lombok.SneakyThrows;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -14,22 +13,19 @@ import org.springframework.stereotype.Component;
 @Scope("prototype")
 public class TriggerCheckThread extends Thread{
     private final ApplicationContextProvider applicationContextProvider;
-    @Setter private Main_game_controller main_game_controller;
+    private final GameModel gameModel;
     private final Collision collision;
     private final LevelChange levelChange;
 
-    public TriggerCheckThread(ApplicationContextProvider applicationContextProvider, Collision collision, LevelChange levelChange) {
+    public TriggerCheckThread(ApplicationContextProvider applicationContextProvider, GameModel gameModel, Collision collision, LevelChange levelChange) {
         this.applicationContextProvider = applicationContextProvider;
+        this.gameModel = gameModel;
         this.collision = collision;
         this.levelChange = levelChange;
     }
 
     @Override
     public void run(){
-        this.setMain_game_controller(this.applicationContextProvider
-                .getApplicationContext()
-                .getBean(Main_game_controller.class));
-
         checkTriggers();
     }
 
@@ -37,13 +33,13 @@ public class TriggerCheckThread extends Thread{
     @SuppressWarnings("all")
     private void checkTriggers(){
         this.setName("trigger");
-        while (this.main_game_controller.isStarted && !this.isInterrupted()) {
-            if (this.main_game_controller.curr_level.getLevelEvents().getLevelEventsVector() != null) {
-                for(Event event : this.main_game_controller.curr_level.getLevelEvents().getLevelEventsVector()){
-                    if (event.check(this.main_game_controller.curr_level)) {
-                        event.ifTrue(this.main_game_controller.curr_level);
+        while (this.gameModel.isStarted && !this.isInterrupted()) {
+            if (this.gameModel.curr_level.getLevelEvents().getLevelEventsVector() != null) {
+                for(Event event : this.gameModel.curr_level.getLevelEvents().getLevelEventsVector()){
+                    if (event.check(this.gameModel.curr_level)) {
+                        event.ifTrue(this.gameModel.curr_level);
                     }else{
-                        event.ifFalse(this.main_game_controller.curr_level);
+                        event.ifFalse(this.gameModel.curr_level);
                     }
                 }
             }
